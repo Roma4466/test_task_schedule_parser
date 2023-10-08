9c44fff0278f778ddfb5daab002e428b877af75f
Roman <roman.malynovsky@gmail.com>
2023-10-08 14:49:49 +0300

    refactored

--- xlxs_parcer.py
+++ xlxs_parcer.py
@@ -1,38 +1,14 @@
-from datetime import datetime
-
 import pandas as pd
 
 from coordinates_getter import get_coordinates_of_column
 import json
 
+from datetime_parcer import format_datetime_for_json
 from read_specialities import read_faculty_and_specialties
 from speciality_extractor import extract_speciality_from_abbreviation
 from str_formatting import remove_spaces_from_start_and_end
-from time_parcer import parce_time
 
-SPECIALITIES = "Спеціальності"
-
-
-# when I convert table from .docx to .xlxs file
-# it converts numbers of week "1-14" into type datetime
-# 01.01.2014, so I have to do it reverse
-def format_datetime(obj):
-    if isinstance(obj, datetime):
-        current_year = datetime.now().year
-        # if second number of weeks was less than 12
-        # it converts first to day of month and second to number of month
-        if obj.year == current_year:
-            return f"{obj.day}-{obj.month}"
-        else:
-            return f"{obj.month}-{obj.year - 2000}"
-    else:
-        return obj
-
-
-def datetime_serializer(obj):
-    if isinstance(obj, datetime):
-        return obj.strftime('%H:%M')
-    raise TypeError("Type not serializable")
+SPECIALITIES_FIELD_NAME = "Спеціальності"
 
 
 def analyze(df):
@@ -52,13 +28,13 @@ def analyze(df):
     # Initialize the final data structure to hold the parsed information
     final_parsed_data = {
         faculty: {
-            SPECIALITIES: {},
+            SPECIALITIES_FIELD_NAME: {},
             "Рік навчання": year_of_study,
             "Роки навчального року": [started_year, started_year + 1],
         }
     }
     for current_specialities in specialities:
-        final_parsed_data[faculty][SPECIALITIES][current_specialities] = {}
+        final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][current_specialities] = {}
     # because of if there is time in 2 cels
     # python reads it like it is only in one cell
     # so I have save last time
@@ -87,7 +63,7 @@ def analyze(df):
 
         group = row[group_column] if pd.notna(row[group_column]) else group
         week = row[week_column] if pd.notna(row[week_column]) else week
-        week = format_datetime(week)
+        week = format_datetime_for_json(week)
         room = row[room_column] if pd.notna(row[room_column]) else room
         # if this cell is empty then
         # there is no need to read further
@@ -108,26 +84,18 @@ def analyze(df):
                     continue
                 current_specialities = extract_speciality_from_abbreviation(discipline_info, specialities)
 
-            # print(f"day of week: {current_day}",
-            #       f"time: {current_time}",
-            #       f"disciple: {discipline}",
-            #       f"teacher: {teacher}",
-            #       f"group: {group}",
-            #       f"week: {week}",
-            #       f"room: {room}")
-
             for speciality in current_specialities:
-                if discipline not in final_parsed_data[faculty][SPECIALITIES][speciality]:
-                    final_parsed_data[faculty][SPECIALITIES][speciality][discipline] = {}
-                if group not in final_parsed_data[faculty][SPECIALITIES][speciality][discipline]:
-                    final_parsed_data[faculty][SPECIALITIES][speciality][discipline][group] = {}
+                if discipline not in final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][speciality]:
+                    final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][speciality][discipline] = {}
+                if group not in final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][speciality][discipline]:
+                    final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][speciality][discipline][group] = {}
 
             start_time_str, end_time_str = current_time.split('-')
             start_time_str = start_time_str.replace(".", ":")
             end_time_str = end_time_str.replace(".", ":")
 
             for speciality in current_specialities:
-                final_parsed_data[faculty][SPECIALITIES][speciality][discipline][group] = {
+                final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][speciality][discipline][group] = {
                     "час початку": start_time_str,
                     "час кінця": end_time_str,
                     "тижні": week,
@@ -140,15 +108,15 @@ def analyze(df):
             teacher = discipline_info.split(", ")[1]
             # now we know faculty, specialty, discipline
             # then let`s initialize field in result file
-            if discipline not in final_parsed_data[faculty][SPECIALITIES][specialities[0]]:
-                final_parsed_data[faculty][SPECIALITIES][specialities[0]][discipline] = {}
+            if discipline not in final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][specialities[0]]:
+                final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][specialities[0]][discipline] = {}
 
-            if group not in final_parsed_data[faculty][SPECIALITIES][specialities[0]][discipline]:
-                final_parsed_data[faculty][SPECIALITIES][specialities[0]][discipline][group] = {}
+            if group not in final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][specialities[0]][discipline]:
+                final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][specialities[0]][discipline][group] = {}
 
             start_time_str, end_time_str = current_time.split('-')
 
-            final_parsed_data[faculty][SPECIALITIES][specialities[0]][discipline][group] = {
+            final_parsed_data[faculty][SPECIALITIES_FIELD_NAME][specialities[0]][discipline][group] = {
                 "час початку": start_time_str,
                 "час кінця": end_time_str,
                 "тижні": week,
@@ -156,8 +124,6 @@ def analyze(df):
                 "день тижня": current_day,
                 "викладач": teacher
             }
-
-    print(final_parsed_data)
 
     json_str = json.dumps(final_parsed_data, indent=4, default=datetime_serializer, ensure_ascii=False)
 
