a764bd0468fedcba1aef551daf3e0646a166a42a
Roman <roman.malynovsky@gmail.com>
2023-10-08 15:59:51 +0300

    added some more test cases

--- speciality_extractor.py
+++ speciality_extractor.py
@@ -1,9 +1,14 @@
+import re
+
+
 def extract_speciality_from_abbreviation(str_input, specialities):
     result = set()
     before_and_after_opening_parenthesis = str_input.split("(")
     filtered_list = [i for i in before_and_after_opening_parenthesis if ")" in i]
     for string in filtered_list:
-        split = string.split(")")[0].split(",")
+        split = string.split(")")[0]
+        # split by "," or "+"
+        split = re.split(r'[,+]', split)
         for abbreviation in split:
             without_non_letters = abbreviation.replace(")", "").replace(".", "").replace(" ", "")
             for speciality in specialities:
