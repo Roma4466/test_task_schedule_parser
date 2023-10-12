08b372e493a4cb7f51ed4cdb16f64edd5fcdc055
Roman <roman.malynovsky@gmail.com>
2023-10-12 21:24:05 +0300

    implemented pandas

--- utils/models/several_majors_schedule.py
+++ utils/models/several_majors_schedule.py
@@ -1,7 +1,7 @@
 import re
 from typing import List
 
-from pandas import DataFrame
+import pandas as pd
 
 from utils.constants import FieldsNames
 from utils.formatting.str_formatting import StringFormatter
@@ -11,53 +11,37 @@ from utils.models.schedule import Schedule
 class SeveralMajorsSchedule(Schedule):
 
     def __init__(self, faculty_name: str, year_of_studying: int, years: List[int], majors: List[str],
-                 schedule_data_frame: DataFrame):
+                 schedule_data_frame: pd.DataFrame):
         super().__init__(faculty_name, year_of_studying, years, majors, schedule_data_frame)
 
     def parse_disciple_cell_text_into_map(self, discipline_info):
-        if len(discipline_info.split(")")) < 2:
-            # sometimes it parses disciple info in 2 cells
-            # in first disciple in second teacher name
-            teacher = StringFormatter.remove_spaces_from_start_and_end(discipline_info)
-            discipline = StringFormatter.remove_spaces_from_start_and_end(self.previous_disciple)
-        else:
-            discipline = StringFormatter.remove_spaces_from_start_and_end(discipline_info.split("(")[0])
-            self.previous_disciple = discipline
-            teacher = StringFormatter.remove_spaces_from_start_and_end(discipline_info.split(")")[1])
-            if not teacher.replace(" ", ""):
-                return
-            self.major = SeveralMajorsSchedule.extract_speciality_from_abbreviation(discipline_info, self._majors)
+        discipline_info_series = pd.Series(discipline_info.split(")"))
+        discipline = StringFormatter.remove_spaces_from_start_and_end(discipline_info_series.iloc[0].split("(")[0])
+        teacher = StringFormatter.remove_spaces_from_start_and_end(discipline_info_series.iloc[-1])
+
+        self.previous_disciple = discipline if len(discipline_info_series) > 1 else self.previous_disciple
+        self.major = SeveralMajorsSchedule.extract_speciality_from_abbreviation(discipline_info, self._majors)
 
         for speciality in self.major:
-            if discipline not in self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][
-                speciality]:
-                self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][speciality][
-                    discipline] = {}
-            if self.group not in \
-                    self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][speciality][
-                        discipline]:
-                self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][speciality][discipline][
-                    self.group] = {}
+            specialities_data = self.final_parsed_data[self._faculty_name].setdefault(
+                FieldsNames.SPECIALITIES_FIELD_NAME, {})
+            major_data = specialities_data.setdefault(speciality, {})
+            discipline_data = major_data.setdefault(discipline, {})
+            group_data = discipline_data.setdefault(self.group, {})
 
-        start_time_str, end_time_str = self.current_time.split('-')
-        start_time_str = start_time_str.replace(".", ":")
-        end_time_str = end_time_str.replace(".", ":")
-        for speciality in self.major:
-            self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][speciality][discipline][
-                self.group] = self.fill_map_information(start_time_str, end_time_str, teacher)
+            start_time_str, end_time_str = self.current_time.split('-')
+            start_time_str = start_time_str.replace(".", ":")
+            end_time_str = end_time_str.replace(".", ":")
+            group_data.update(self.fill_map_information(start_time_str, end_time_str, teacher))
 
     @staticmethod
     def extract_speciality_from_abbreviation(str_input: str, majors: List[str]):
         result = set()
-        before_and_after_opening_parenthesis = str_input.split("(")
-        filtered_list = [i for i in before_and_after_opening_parenthesis if ")" in i]
-        for string in filtered_list:
-            split = string.split(")")[0]
-            # split by "," or "+"
-            split = re.split(r'[,+]', split)
-            for abbreviation in split:
-                without_non_letters = abbreviation.replace(")", "").replace(".", "").replace(" ", "")
-                for speciality in majors:
-                    if without_non_letters in speciality.lower():
-                        result.add(speciality)
+        abbreviations = pd.Series(re.split(r'[,+]', str_input.split("(")[-1].split(")")[0]))
+        abbreviations = abbreviations.str.replace(")", "").str.replace(".", "").str.replace(" ", "").str.lower()
+
+        for speciality in majors:
+            if abbreviations.isin([speciality.lower()]).any():
+                result.add(speciality)
+
         return result
--- utils/models/single_major_schedule.py
+++ utils/models/single_major_schedule.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pandas import DataFrame
+import pandas as pd
 
 from utils.constants import FieldsNames
 from utils.formatting.str_formatting import StringFormatter
@@ -10,28 +10,24 @@ from utils.models.schedule import Schedule
 class SingleMajorSchedule(Schedule):
 
     def __init__(self, faculty_name: str, year_of_studying: int, years: List[int], majors: List[str],
-                 schedule_data_frame: DataFrame):
+                 schedule_data_frame: pd.DataFrame):
         super().__init__(faculty_name, year_of_studying, years, majors, schedule_data_frame)
 
     def parse_disciple_cell_text_into_map(self, discipline_info):
-        discipline = StringFormatter.remove_spaces_from_start_and_end(discipline_info.split(", ")[0])
-        teacher = StringFormatter.remove_spaces_from_start_and_end(discipline_info.split(", ")[1]) if len(
-            discipline_info.split(", ")) > 1 else "???"
-        # now we know faculty, specialty, discipline
-        # then let`s initialize field in result file
-        if discipline not in self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][
-            self._majors[0]]:
-            self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][self._majors[0]][
-                discipline] = {}
+        # Split and clean up the discipline and teacher information
+        discipline_info_series = pd.Series(discipline_info.split(", "))
+        discipline_info_series = discipline_info_series.apply(StringFormatter.remove_spaces_from_start_and_end)
 
-        if self.group not in \
-                self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][self._majors[0]][
-                    discipline]:
-            self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][self._majors[0]][
-                discipline][
-                self.group] = {}
+        discipline = discipline_info_series.iloc[0]
+        teacher = discipline_info_series.iloc[1] if len(discipline_info_series) > 1 else "???"
 
+        # Initialize the nested dictionaries if they don't exist
+        faculty_data = self.final_parsed_data.setdefault(self._faculty_name, {})
+        specialities_data = faculty_data.setdefault(FieldsNames.SPECIALITIES_FIELD_NAME, {})
+        major_data = specialities_data.setdefault(self._majors[0], {})
+        discipline_data = major_data.setdefault(discipline, {})
+        group_data = discipline_data.setdefault(self.group, {})
+
+        # Fill in the information
         start_time_str, end_time_str = self.current_time.split('-')
-
-        self.final_parsed_data[self._faculty_name][FieldsNames.SPECIALITIES_FIELD_NAME][self._majors[0]][discipline][
-            self.group] = self.fill_map_information(start_time_str, end_time_str, teacher)
+        group_data.update(self.fill_map_information(start_time_str, end_time_str, teacher))
